fe0cea79a1238186814580dd6b7feda3e7b62772
phsete <philipp-sebastian.tebbe@udo.edu>
2023-10-11 14:50:01 +0200

    add config file

--- node.py
+++ node.py
@@ -3,17 +3,15 @@ import websockets
 import json
 import log
 import requests
+import configparser
 
-ESP32_VID_PID = "10c4:ea60"
-LOGGER_VERSION = "latest"
 URL_LATEST = "https://github.com/phsete/ESPNOWLogger/releases/latest/download/sender.bin"
-URL_VERSION = "https://github.com/phsete/ESPNOWLogger/releases/download/" + LOGGER_VERSION + "/sender.bin"
 
 async def process_message(message):
     data = json.loads(message)
     print(f"received message of type {data['type']}")
     if data["type"] == "start_test":
-        (collected_power_samples, collected_data_samples) = log.start_test(esp32_vid_pid=ESP32_VID_PID, flash=True)
+        (collected_power_samples, collected_data_samples) = log.start_test(esp32_vid_pid=config["node"]["ESP32VidPid"], flash=True)
         return json.dumps({"power_samples": collected_power_samples._getvalue(), "data_samples": collected_data_samples._getvalue()})
     else:
         return "OK"
@@ -28,14 +26,17 @@ async def respond(websocket):
     print(f">>> {outgoing}")
 
 async def main():
-    async with websockets.serve(respond, "0.0.0.0", 8765):
+    async with websockets.serve(respond, "0.0.0.0", config["general"]["WebsocketPort"]):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
-    if(LOGGER_VERSION == "latest"):
+    config = configparser.ConfigParser()
+    config.read("config.toml")
+
+    if(config["node"]["LoggerVersion"] == "latest"):
         url = URL_LATEST
     else:
-        url = URL_VERSION
+        url = "https://github.com/phsete/ESPNOWLogger/releases/download/" + config["node"]["LoggerVersion"] + "/sender.bin"
         
     response = requests.get(url)
     if(response.ok):
